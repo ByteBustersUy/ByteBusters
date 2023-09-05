a9493fe67a3e4cd83b6d61f3e49b91c6cb880234
[damian despan] <[damiandespan@gmail.com]>
2023-09-05 14:05:08 -0300

    guarda en el localstorage: habia que convertir el carrito a una cadena

--- Ecommerce/js/functions/agregar-carrito.js
+++ Ecommerce/js/functions/agregar-carrito.js
@@ -1,40 +1,38 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var carrito = [];
-    //Guarda el carrito en el LocalStorage
-    setTimeout(()=>{
-        document.querySelectorAll(".agregar-carrito").forEach((btn) =>{
-        
-        btn.addEventListener("click", () => {
-            var id = btn.id;
-            console.log(id);
-            var cantidad = 1;
-            agregarProducoAlCarrito(id, cantidad)
-        });
+
+let carrito = [];
+
+// Carga 'carrito' del localStorage esto iria en la parte de mostrar carrito
+/*const carritoGuardado = localStorage.getItem("carrito");
+if (carritoGuardado) {
+  carrito = JSON.parse(carritoGuardado);
+}*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  //Guarda el carrito en el LocalStorage
+  setTimeout(() => {
+    document.querySelectorAll(".agregar-carrito").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const id = btn.id;
+        console.log(id);
+        const cantidad = 1;
+        agregarProducoAlCarrito(id, cantidad);
+      });
     });
+  }, 500);
 
-}, 500);
-
-    //carga carrito del localStorage
-   /* var carritoGuardado = localStorage.getItem("carrito");
-    if(carritoGuardado){
-        carrito = JSON.parse(carritoGuardado);
-        
-
-    }*/
-
-    function agregarProducoAlCarrito(id, cantidad){
-        var productoExistente = carrito.find((producto) => producto.id === id);
-        if(productoExistente){
-            productoExistente.cantidad += cantidad;
-        }else{
-            carrito.push({
-                id: id,
-                cantidad: cantidad,
-            });
-        }
-        console.log(carrito)
+  function agregarProducoAlCarrito(id, cantidad) {
+    const productoExistente = carrito.find((producto) => producto.id === id);
+    if (productoExistente) {
+      productoExistente.cantidad += cantidad;
+    } else {
+      carrito.push({
+        id: id,
+        cantidad: cantidad,
+      });
     }
+    console.log(carrito);
 
+    const carritoJSON = JSON.stringify(carrito);
+    localStorage.setItem("carrito", carritoJSON);
+  }
 });
-
-
--- Ecommerce/js/functions/carrito.js
+++ Ecommerce/js/functions/carrito.js
@@ -1,70 +1,5 @@
+
 window.addEventListener('load', function() {
-    var productos = $.parseJSON(localStorage.getItem(localStorage.key(0)))
-
-    for(var i = 0; i < localStorage.length; i++) {              
-      var clave = localStorage.key(i);
-      var data = $.parseJSON(localStorage.getItem(clave));
-
-      const newElement = document.createElement("div");
-      const divnombreproduc = document.createElement("div");
-      const divproducto = document.createElement("div");
-      const imgproducto = document.createElement("img");
-      const divcantidad = document.createElement("div");
-      const nombreProduc = document.createElement("p");
-      const btnscantidad = document.createElement("div");
-      const inputnum = document.createElement("input");
-      const inputbtnmas = document.createElement("input");
-      const inputbtnmenos = document.createElement("input");
-      const precioProducto = document.createElement("p");
-      const btneliminar = document.createElement("button");
-      //LES OTORGO UNA CLASE A LOS ELEMENTOS
-      newElement.classList.add("col-12");
-      newElement.classList.add("div-producto");
-      nombreProduc.classList.add("nombre-produc"),
-      divnombreproduc.classList.add("div-nombre-produc");
-      precioProducto.classList.add("precio-produc")
-      divproducto.classList.add("div-dato-produc");
-      divcantidad.classList.add("cantidad");
-      imgproducto.classList.add("img-producto");
-      btneliminar.classList.add("btn-eliminar");
-      btnscantidad.classList.add("btns-cantidad");
-      inputnum.classList.add("input-num")
-    
-      //AGREGO PROPIEDADES A LOS ELEMENTOS 
-      nombreProduc.textContent = data[id].nombre;
-      imgproducto.src = "./../images/"+data[id].imagen;
-      inputbtnmenos.type="button";
-      inputbtnmenos.value="-";
-      inputnum.type="number";
-      inputnum.value=1;
-      inputbtnmas.type="button";
-      inputbtnmas.value="+";
-      precioProducto.textContent = "Precio: $"+data[id].precio;
-      btneliminar.innerHTML ='<i class="fa-solid fa-trash"></i>';
-      
-      //SE AÑADEN LOS ELEMENTOS AL DOCUMENTO HTML 
-      document.querySelector(".row").appendChild(newElement);
-      newElement.appendChild(imgproducto);
-      newElement.appendChild(divproducto);
-      divnombreproduc.appendChild(nombreProduc);
-      divproducto.append(divnombreproduc);
-      btnscantidad.append(inputbtnmenos);
-      btnscantidad.append(inputnum);
-      btnscantidad.append(inputbtnmas);
-      divcantidad.append(btnscantidad);
-      divcantidad.appendChild(precioProducto);
-      divcantidad.append(btneliminar);
-      divproducto.appendChild(divcantidad);
-     }
- })
- .catch(error => {
-     console.error("error",error)
- });
-    
-//});
-
-
-/*window.addEventListener('load', function() {
    console.log("funciona")
    fetch("../../api/mostrar.php")
    .then(response => response.json()) 
@@ -127,4 +62,4 @@ window.addEventListener('load', function() {
        console.error("error",error)
    });
 });
-*/
+
