ea5e38d9716c066fa41edde6be3e9b82a4d3945a
fdefortunyDev <fdefortuny@gmail.com>
2023-09-07 20:41:00 -0300

    refactor checked roles en permisos

--- Backoffice/src/modules/users/abm-permisos.php
+++ Backoffice/src/modules/users/abm-permisos.php
@@ -3,35 +3,33 @@ require realpath(dirname(__FILE__)) . "/../../repository/users.repository.php";
 
 function getAllPermissionsDataTableHTML(): string
 {
+    require realpath(dirname(__FILE__)) . "/../../utils/actions.php";
     $permissions = findAllPermissions();
     $totalOfPermissions = count($permissions);
-
     $data = "";
+
     for ($i = 0; $i < $totalOfPermissions; $i++) {
-        $settedRolId = findOneRolIdByAction($permissions[$i]["accion"]);
-        if ($settedRolId == 1) {
-            $data .= '<tr class="user-select-none align-middle">
-                        <td class="first-in-table">' . $permissions[$i]["accion"] . '</td>
-                        <td>' . $permissions[$i]["ruta"] . '</td>
-                        <td>
-                            <input type="checkbox" class="chkbox-roles" checked=true name="checkAdmin" id="checkAdmin">
-                        </td>
-                        <td>
-                        <input type="checkbox"  class="chkbox-roles" name="checkVendedor" id="checkVendedor">
-                        </td>
-                    </tr>';
-        } else if ($settedRolId == 2) {
-            $data .= '<tr class="user-select-none align-middle">
-                        <td class="first-in-table">' . $permissions[$i]["accion"] . '</td>
-                        <td>' . $permissions[$i]["ruta"] . '</td>
-                        <td>
-                            <input type="checkbox" class="chkbox-roles" name="checkAdmin" id="checkAdmin">
-                        </td>
-                        <td>
-                            <input type="checkbox"  class="chkbox-roles" checked=true name="checkVendedor" id="checkVendedor">
-                        </td>
-                        </tr>';
+        $settedRolesId = findRolesIdByAction($permissions[$i]["accion"]);
+        $activeAdmin = '';
+        $activeVendedor = '';
+
+        foreach ($settedRolesId as $rolId) {
+            if (implode($rolId) == '1') {
+                $activeAdmin = 'checked';
+            } else if (implode($rolId) == '2') {
+                $activeVendedor = 'checked';
+            }
         }
+        $data .= '<tr id=' . array_search($permissions[$i]["accion"], $actions) . ' class="user-select-none align-middle">
+                    <td class="first-in-table">' . $permissions[$i]["accion"] . '</td>
+                    <td>' . $permissions[$i]["ruta"] . '</td>
+                    <td>
+                        <input type="checkbox" class="chkbox-roles" ' . $activeAdmin . ' name="checkAdmin" id="checkAdmin">
+                    </td>
+                    <td>
+                        <input type="checkbox"  class="chkbox-roles" ' . $activeVendedor . ' name="checkVendedor" id="checkVendedor">
+                    </td>
+                </tr>';
     }
     return $data;
 }
