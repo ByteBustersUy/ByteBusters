2210e6eff6ed15f5443c7eba0f373fe190140829
angellanzi <angellanzi.sl@gmail.com>
2023-09-08 19:25:56 -0300

    redireccionamiento a detalle-de-producto y retoque de carrito

--- Ecommerce/js/functions/productos.js
+++ Ecommerce/js/functions/productos.js
@@ -7,12 +7,14 @@ window.addEventListener("load", function () {
 				divPrductoSinPromo.innerHTML += 
        `<div class="list-group"> 
           <div class="d-flex list-body">
+          <a class="" href="pages/carrito.html">
             <img class="img-list" src="./images/${data[id].imagen}" class="card-img-top" alt="...">
             <div class="d-block list-content">
             <h5>${data[id].nombre}</h5>
             <h4>$${data[id].precio}</h4>
             <a id="${data[id].id}" class="btn btn-agregar agregar-carrito">Agregar al carrito</a>
             </div>
+          </a>
           </div>
         </div>`;
 			}
