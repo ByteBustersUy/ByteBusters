b44ba448a710268a4789cc842c787f190d1927d3
[Damian Despan] <[damiandespan@gmail.com]>
2023-09-13 16:33:55 -0300

    comentarios en footer

--- Ecommerce/js/index.js
+++ Ecommerce/js/index.js
@@ -1,10 +1,6 @@
 window.addEventListener("load", function () {
-<<<<<<< HEAD
-	fetch("../api/productos-promo.php")
-=======
 	//Productos promocionados
 	fetch("../api/mostrar.php")
->>>>>>> 09c5a47fde173dd82bf251b2b67145b7ed38e7ae
 		.then((response) => response.json())
 		.then((data) => {
 			for (let id = 0; id < data.length; id++) {
@@ -29,7 +25,6 @@ window.addEventListener("load", function () {
 	fetch("../api/productos.php")
 		.then((response) => response.json())
 		.then((data) => {
-<<<<<<< HEAD
 			const divDatosEmpresa = document.getElementById("divDatosEmpresa");
 			divDatosEmpresa.innerHTML += `
             <p>${data[0].calle} ${data[0].numero} - ${data[0].ciudad}</p>
@@ -40,27 +35,6 @@ window.addEventListener("load", function () {
 		
     });
 });
-=======
-			for (let id = 0; id < data.length; id++) {
-				const divPrductoSinPromo = document.getElementById(
-					"productos-sin-promo"
-				);
-				divPrductoSinPromo.innerHTML += `
-				<div class="list-group"> 
-        			<div class="d-flex list-body">
-        				<a class="" href="pages/carrito.html">
-            				<img class="img-list" src="./images/${data[id].imagen}" class="card-img-top" alt="...">
-            				<div class="d-block list-content">
-            					<h5>${data[id].nombre}</h5>
-            					<h4>$${data[id].precio}</h4>
-            					<a id="${data[id].id}" class="btn btn-agregar agregar-carrito">Agregar al carrito</a>
-            				</div>
-        				</a>
-       		 		</div>
-        		</div>`;
-			}
-		});
->>>>>>> 09c5a47fde173dd82bf251b2b67145b7ed38e7ae
 
 	//Buscador inteligente
 	const inputSearch = document.getElementById("navSearch");
@@ -83,4 +57,4 @@ window.addEventListener("load", function () {
 				console.log(sugerencias);
 			});
 	});
-});
+
