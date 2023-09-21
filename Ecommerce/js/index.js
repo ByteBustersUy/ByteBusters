710e6dc56796ee01e4ad869d77f38dfaae164d83
[Damian Despan] <[damiandespan@gmail.com]>
2023-09-21 14:54:33 -0300

    categorias andando en home

--- Ecommerce/js/index.js
+++ Ecommerce/js/index.js
@@ -139,39 +139,37 @@ window.addEventListener("load", function () {
 				const id = element.target.id;
 				console.log("id de categoría: ", id);
 
-				fetch("../../api/productos-categorias.php", {
-					method: "POST",
+				fetch("../api/productos-categorias.php?id=" + id, {
+					method: "GET",
 					headers: {
-						"Content-Type": "application/json",
+					  "Content-Type": "application/json",
 					},
-					body: JSON.stringify({ id }),
-				})
+				  })
 					.then((response) => {
-						console.log("status: ", response.status);
-						response.json();
+					  console.log("status: ", response.status);
+					  return response.json();
 					})
 					.then((data) => {
-						//cuando data tenga algo util, descomentamos lo otro, pa no tener varios fallos a la vez y poder debugear
-						console.log("data tiene: ", data );
-						// 		let contenido = "";
-						// 		for (let id = 0; id < data.length; id++) {
-						// 			contenido += `
-						// <div>
-						// 	<div class="card h-100 produc-promo" >
-						// 		<a class="ir-detalle-producto" href="pages/carrito.html">
-						// 			<img src="./images/${data[id].imagen} " class="card-img-top" alt="...">
-						// 			<div class="card-body">
-						// 				<h4>$${data[id].precio}</h4>
-						// 				<h5>${data[id].nombre}</h5>
-						// 			</div>
-						// 		</a>
-						// 		<a id="${data[id].id}" class="btn btn-agregar agregar-carrito">Agregar al carrito</a>
-						// 	</div>
-						// </div>`;
-						// 		}
-						// 		category = data.length;
-						// 		divProducPromo.innerHTML = contenido;
-						// 		cargarBotones(category);
+					  console.log("data tiene: ", data);
+								let contenido = "";
+								for (let id = 0; id < data.length; id++) {
+								 			contenido += `
+						 <div>
+						 	<div class="card h-100 produc-promo" >
+						 		<a class="ir-detalle-producto" href="pages/carrito.html">
+						 			<img src="./images/${data[id].imagen} " class="card-img-top" alt="...">
+						 			<div class="card-body">
+						 				<h4>$${data[id].precio}</h4>
+						 				<h5>${data[id].nombre}</h5>
+						 			</div>
+						 		</a>
+						 		<a id="${data[id].id}" class="btn btn-agregar agregar-carrito">Agregar al carrito</a>
+						 	</div>
+						 </div>`;
+						 		}
+						 		category = data.length;
+						 		divProducPromo.innerHTML = contenido;
+								cargarBotones(category);
 					})
 					.catch((error) => {
 						console.log(error);
