44a5f3b6aaa5158e80cbd3bc5b6aa89d0df8ddfc
[Damian Despan] <[damiandespan@gmail.com]>
2023-09-26 13:25:35 -0300

    Script buscador inteligente en listar

--- Ecommerce/js/index.js
+++ Ecommerce/js/index.js
@@ -59,7 +59,7 @@ window.addEventListener("load", function () {
 				cards += `
     			<div>
         			<div class="card h-100 produc-promo" >
-						<a class="ir-detalle-producto" href="pages/carrito.html">
+						<a class="ir-detalle-producto" href="pages/detalleProducto.html?id=${id}">
             				<img src="./images/${data[id].imagen} " class="card-img-top" alt="...">
             				<div class="card-body">
                 				<h4>$${data[id].precio}</h4>
