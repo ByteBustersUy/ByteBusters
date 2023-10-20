2100c4409d4c1e7a686004135b93f25f27fd828b
fdefortunyDev <fdefortuny@gmail.com>
2023-10-20 20:12:21 -0300

    fix search

--- Ecommerce/js/functions/buscador-inteligente.js
+++ Ecommerce/js/functions/buscador-inteligente.js
@@ -1,9 +1,16 @@
+const currentURL = window.location.href;
+const urlParts = currentURL.split("/");
+const currentDir = urlParts[urlParts.length - 2];
+let relativePath;
+if (currentDir == "pages") relativePath = "..";
+if (currentDir == "Ecommerce") relativePath = ".";
+
 document.addEventListener("DOMContentLoaded", function () {
 	setTimeout(function () {
 		const inputSearch = document.getElementById("navSearch");
 		inputSearch.addEventListener("keyup", () => {
 			let cantProduc;
-			fetch(`../../api/search.php?p=`, {
+			fetch(`${relativePath}/api/search.php?p=`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
@@ -12,12 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
 			})
 				.then((response) => response.json())
 				.then((data) => {
+					console.log(data);
 					let contenido = "";
 					for (let id = 0; id < data.length; id++) {
 						contenido += `
                 <div class="row cardProd">
                 <div class="col-md-12 d-flex">
-                  <a href=""><img src="../images/${data[id].imagen}" class="card-img-top img-producto-lista" alt="10"></a>
+                  <a href=""><img src="./images/${data[id].imagen}" class="card-img-top img-producto-lista" alt="10"></a>
                   <div>
                     <a class="aNomb" href="">
                       <h3>${data[id].nombre}</h3>
@@ -31,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
 					}
 
 					cantProduc = data.length;
-					document.querySelector(".container-sm").innerHTML = contenido;
+					document.querySelector(".contenedor-search").innerHTML = contenido;
 				});
 		});
 	}, 1000);
