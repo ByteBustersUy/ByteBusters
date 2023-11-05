9ca8f410124267ed01b9152ed293b3c9c9092f72
fdefortunyDev <fdefortuny@gmail.com>
2023-11-05 15:28:35 -0300

    fix pdf

--- Backoffice/src/repository/download.repository.php
+++ Backoffice/src/repository/download.repository.php
@@ -9,7 +9,7 @@ function findAllProductData(): array
         return $registros;
     } catch (Exception $e) {
         $con->close();
-        die("ERROR SQL in findAllDataProduct(): " . $e->getMessage());
+        die("ERROR SQL in findAllProductData(): " . $e->getMessage());
     }
 }
 function findProductDataByDateOfPromotion($fechaInicio,$fechaFin)
@@ -17,7 +17,7 @@ function findProductDataByDateOfPromotion($fechaInicio,$fechaFin)
 
     require realpath(dirname(__FILE__)) . "/../db/conexion.php";
     try{
-        $resultado = $con->query("  SELECT nombre,descripcion,precio 
+        $resultado = $con->query("  SELECT id,nombre,descripcion,precio 
                                     FROM PRODUCTOS 
                                     WHERE id 
                                     IN(
@@ -27,11 +27,31 @@ function findProductDataByDateOfPromotion($fechaInicio,$fechaFin)
                                         AND fecha <= '$fechaFin')
                                         ");
         $registros = $resultado->fetchAll(PDO::FETCH_ASSOC);
-       return $registros;
+        return $registros;
     }catch(Exception $e) {
         $con->close();
-        die("ERROR SQL in findAllDataProduct(): " . $e->getMessage());
+        die("ERROR SQL in findProductDataByDateOfPromotion(): " . $e->getMessage());
     }
+}
 
-
+function findDiscountByProductId(int $productId)
+{
+    require realpath(dirname(__FILE__)) . "/../db/conexion.php";
+    try{
+        $res = $con->prepare("SELECT descuento
+                                FROM PROMOCIONES 
+                                WHERE id 
+                                IN(
+                                    SELECT PROMOCIONES_id 
+                                    FROM PRODUCTOS_has_PROMOCIONES
+                                    WHERE PRODUCTOS_id = :productId 
+                                    )
+                            ");
+        $res->execute([":productId" => $productId]);
+        $reg = $res->fetch(PDO::FETCH_ASSOC);
+        return $reg ? $reg["descuento"] : 0;
+    }catch(Exception $e) {
+        $con->close();
+        die("ERROR SQL in findDiscountByProductId(): " . $e->getMessage());
+    }
 }
