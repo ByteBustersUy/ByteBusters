5a29ee48a784130c79bac99c11bebd38f6ad2513
fdefortunyDev <fdefortuny@gmail.com>
2023-11-15 20:12:44 -0300

    fix consulta productos no promo

--- api/productos-no-promo.php
+++ api/productos-no-promo.php
@@ -1,7 +1,12 @@
 <?php
 include "./db/conexion.php";
 
-$res = $con->query("SELECT * FROM productos WHERE activo = 1 LIMIT 10");
+$res = $con-> query("SELECT P.*
+                    FROM PRODUCTOS P
+                    LEFT JOIN PRODUCTOS_has_PROMOCIONES PP ON P.id = PP.PRODUCTOS_id
+                    WHERE P.activo = 1
+                    AND PP.PRODUCTOS_id IS NULL;"
+                    );
 $reg = $res->fetchAll(PDO::FETCH_ASSOC);
 
 header("Content-Type: application/json");
